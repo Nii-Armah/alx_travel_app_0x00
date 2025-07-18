b93e3fb8088464e0278517cfcf592a8b092f92da
Nii-Armah <stanleyniiarmah7@gmail.com>
2025-07-18 07:52:33 +0000

    Created Listing model
    Manages data of all listings within the system.

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -3,6 +3,9 @@ Data models for listings application.
 
 User:
     A user of the system.
+
+Listing:
+    A bookable travel-related item or service.
 """
 
 from django.contrib.auth.models import AbstractUser
@@ -33,4 +36,37 @@ class User(AbstractUser):
         max_length=10,
         choices=Role.choices,
         help_text='Type of user'
+    )
+
+
+class Listing(models.Model):
+    """A bookable travel-related item or service."""
+    class Meta:
+        db_table = 'listings'
+
+    class Type(models.TextChoices):
+        """Types of listings."""
+        APARTMENT = 'APARTMENT', 'Apartment'
+        ROOM = 'ROOM', 'Room'
+        TOUR = 'TOUR', 'Tour'
+        Car = 'Car', 'Car'
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text='Unique identification of listing'
+    )
+
+    title = models.CharField(max_length=100, help_text='Title or name of listing')
+    description = models.TextField(help_text='Description of listing')
+    location = models.CharField(max_length=100, help_text='Location of listing')
+    price = models.DecimalField(decimal_places=2, max_digits=10, help_text='Price of listing')
+    type = models.CharField(max_length=100, help_text='Type of listing')
+
+    host = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='listings',
+        help_text='Host of listing'
     )
